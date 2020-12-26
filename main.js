a9ff34a16d4d1a7ac10b368b01880c75d7510a76
Vlad Hapon <vladhpn@gmail.com>
2020-12-26 14:38:15 +0200

    на проверку

---
+++ main.js
@@ -0,0 +1,66 @@
+import images from "./gallery-items.js";
+
+const refs = {
+  gallery: document.querySelector(".js-gallery"),
+  image: document.createElement("img"),
+  lightbox: document.querySelector(".lightbox"),
+  btn: document.querySelector('[data-action="close-lightbox"]'),
+  modal: document.querySelector(".lightbox__content"),
+  lightbox__image: document.querySelector(".lightbox__image"),
+};
+
+const createGalleryItem = ({ preview, original, description }) =>
+  `<li class="gallery__item">
+<a
+  class="gallery__link"
+  href=${original}
+>
+  <img
+    class="gallery__image"
+    src=${preview}
+    data-source=${original}
+    alt=${description}
+  />
+</a>
+</li>`;
+const galleryMarkup = images.reduce(
+  (acc, item) => acc + createGalleryItem(item),
+  ""
+);
+refs.gallery.insertAdjacentHTML("afterbegin", galleryMarkup);
+refs.image.classList.add("gallery__image");
+
+refs.gallery.addEventListener("click", onGalleryClick);
+refs.btn.addEventListener("click", onClickHandlerClose);
+refs.modal.addEventListener("click", closeLightbox);
+
+function onGalleryClick(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  if (event.target.nodeName === "IMG") {
+    refs.lightbox.classList.add("is-open");
+    refs.lightbox__image.src = event.target.getAttribute("data-source");
+    refs.lightbox__image.alt = event.target.alt;
+  }
+  window.addEventListener("keyup", clickKey);
+}
+
+function onClickHandlerClose(event) {
+  refs.lightbox.classList.remove("is-open");
+  refs.lightbox__image.src = "";
+  refs.lightbox__image.alt = "";
+  window.removeEventListener("keyup", clickKey);
+}
+
+function closeLightbox(event) {
+  if (event.target === event.currentTarget) {
+    onClickHandlerClose();
+  }
+}
+function clickKey(event) {
+  if (event.code === "Escape") {
+    onClickHandlerClose();
+  }
+}
